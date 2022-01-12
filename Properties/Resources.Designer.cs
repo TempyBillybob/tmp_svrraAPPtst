7a30239c3a06d9c5cc32d0cafee4b92462107dcb
Billybob <danktempbillybob@gmail.com>
2022-01-11 23:24:04 -0800

    changed some names around

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SAR_Server_App.Properties {
+namespace WC.SARS.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SAR_Server_App.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAR_Server_App.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WC.SARS.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
